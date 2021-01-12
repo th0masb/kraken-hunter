493c83095d4a29677c6e9d9fb8aae335d1febda2
th0masb <tomoliverball@gmail.com>
2021-01-12 22:53:00 +0000

    [ISSUE-1] Implemented Ping, Subscribe, Unsubscribe request types

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,53 @@
-use websocket::{ClientBuilder, Message};
-use anyhow::{Result, Error, anyhow};
+mod req;
+
+use crate::req::{Subscription, WsReq};
+use anyhow::{anyhow, Error, Result};
+use websocket::client::sync::Client;
+use websocket::websocket_base::stream::sync::NetworkStream;
 use websocket::ws::dataframe::DataFrame;
-use bytes::Bytes;
+use websocket::{ClientBuilder, Message};
+
+const ENDPOINT: &'static str = "wss://ws.kraken.com";
+
+pub struct Kraken {
+    inner: Client<Box<dyn NetworkStream + Send>>,
+}
+
+impl Kraken {
+    pub fn new() -> Result<Kraken> {
+        Ok(Kraken {
+            inner: ClientBuilder::new(ENDPOINT)?.connect(None)?,
+        })
+    }
+
+    pub fn send_req(&mut self, req: WsReq) -> Result<()> {
+        self.inner
+            .send_message(&Message::text(serde_json::to_string(&req)?))
+            .map_err(Error::from)
+    }
+}
 
 fn main() -> Result<()> {
-    let public_endpoint = "wss://ws.kraken.com";
-    let mut client = ClientBuilder::new(public_endpoint)?
-        .connect(None)?;
+    let mut client = Kraken::new()?;
+    client.send_req(WsReq::Ping { request_id: Some(10) })?;
+    println!("{}", String::from_utf8(client.inner.recv_message()?.take_payload())?);
+    println!("{}", String::from_utf8(client.inner.recv_message()?.take_payload())?);
 
-    client.send_message(&Message::text(r#"
-        {
-            "event": "subscribe",
-            "pair": [
-                "XBT/USD"
-            ],
-            "subscription": {
-                "name": "ticker"
-            }
-        }"#))?;
+    client.send_req(WsReq::Subscribe {
+        request_id: Some(12),
+        pair: vec!["ETH/USD".to_string()],
+        subscription: Subscription::Ticker,
+    })?;
 
-    for i in 0..10 {
-        let message = Bytes::from(client.recv_message()?.take_payload());
-        println!("{} {:?}", i, message)
+    for message in client.inner.incoming_messages().take(10).filter_map(|x| x.ok()) {
+        println!("{:?}", message)
     }
+
+    client.send_req(WsReq::Unsubscribe {
+        request_id: Some(12),
+        pair: vec!["ETH/USD".to_string()],
+        subscription: Subscription::Ticker,
+    })?;
+
     Ok(())
 }
